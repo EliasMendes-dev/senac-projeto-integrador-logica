3a74c6499ffe616dbda97e9ef6170e95a137fa42
José Elias Herminio Mendes <eliasmendes2908@gmail.com>
2026-08-19 10:55:59 -0300

    feat exemplo java com livros favoritos

---
+++ Faculdade/Algoritmo e Programação/19-08-2026/Exemplo1.java
@@ -0,0 +1,10 @@
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Esse algoritmo vai listar meus 5 livros favoritos.\n");
+        String[] livros = { "O Horror de Dunwich", "Dagon", "O Chamado de Cthulhu", "A pirâmide Vermelha", "O Trono de Fogo" };
+
+        for (int i = 0; i < livros.length; i++) {
+            System.out.println((i + 1) + "-) " + livros[i]);
+        }
+    }
+}
